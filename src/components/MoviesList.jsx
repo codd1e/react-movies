8f2030afba9828d68f5f41009a064a9288de104e
codd1e <andreinik32@gmail.com>
2022-10-06 21:21:59 +0300

    poject complete

---
+++ src/components/MoviesList.jsx
@@ -0,0 +1,17 @@
+import Movie from './Movie'
+
+function MoviesList(props) {
+    const { movies = [] } = props;
+
+    return (
+        <div className='movies'>
+            {movies.length ? (
+                movies.map((movie) => <Movie key={movie.imdbID} {...movie} />)
+            ) : (
+                <h3>Nothing found</h3>
+            )}
+        </div>
+    );
+}
+
+export default MoviesList;
